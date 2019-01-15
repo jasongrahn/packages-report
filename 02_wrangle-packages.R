720ccd58b153832f065d056902bf9b2a7f376079
Jason Grahn <jasongrahn@gmail.com>
2019-01-15 22:53:36 +0000

    two scripts

---
+++ 02_wrangle-packages.R
@@ -0,0 +1,39 @@
+## remember to restart R here!
+
+## create a data frame by reading from data/installed-packages.csv
+## hint: readr::read_csv() or read.csv()
+## idea: try using here::here() to create the file path
+library(readr)
+installed_packages <- read_csv(file = here::here("data/installed-packages.csv"))
+
+## filter out the base and recommended packages
+library(tidyverse)
+
+## keep the variables Package and Built
+## if you use dplyr, code like this will work:
+apt <- installed_packages %>%
+  filter(is.na(Priority)) %>%
+  select(Package, Built)
+
+## write this new, smaller data frame to data/add-on-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+readr::write_csv(x = apt,
+                 path = here::here("/data/add-on-packages.csv"))
+
+## make a frequency table of package by the version in Built
+## if you use dplyr, code like this will work:
+apt_freqtable <- apt %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
+apt_freqtable
+## write this data frame to data/add-on-packages-freqtable.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+readr::write_csv(x = apt,
+                 path = here::here("/data/add-on-packages-freqtable.csv"))
+
+## YES overwrite the files that are there now
+## they are old output from me (Jenny)
+## they are just examples
